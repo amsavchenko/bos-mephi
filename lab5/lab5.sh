09d4534c5e35304342e1f2e03c18019dc6dd8e0b
amsavchenko <savchenko_a@icloud.com>
2019-12-24 07:38:34 -0500

    update lab5.sh

--- lab5/lab5.sh
+++ lab5/lab5.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-if [ $1 == $2 ]; then
-	echo "$3"
-fi
+file="lab5.out"
+ps -eo euid,ruid,comm | tail -n +2  >"$file"
+exec 0<lab5.out # связываем дескриптор 0 с файлом в который записали
+
+while read euid ruid name 
+do
+	if [[ $euid != $ruid ]]; then
+		echo "$name has diffs between real uid and effective uid"
+	fi
+	
+
+done
+rm lab5.out
